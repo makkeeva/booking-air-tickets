d440a261db567ad81b16bc4ecd5e9ae1f743a9ee
Dasha Makeeva <1782842dm@gmail.com>
2023-03-16 02:57:09 +0300

    Initial commit

---
+++ src/main/java/com/booking/BookingAirTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAirTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAirTicketsApplication.class, args);
+	}
+
+}
