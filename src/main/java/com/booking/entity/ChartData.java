b134da4bd6302f2b091dbd706381827fd08e60a4
Dasha Makeeva <1782842dm@gmail.com>
2023-04-25 14:46:37 +0300

    Added DTO entity for chart

---
+++ src/main/java/com/booking/entity/ChartData.java
@@ -0,0 +1,17 @@
+package com.booking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChartData {
+    private String[] axisX;
+    private int[] data;
+}
